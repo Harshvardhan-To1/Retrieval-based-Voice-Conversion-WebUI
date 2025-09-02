8e3a261e8bb6ee8b9b4b8299091b3cdb5b20df07
Harshvardhan Choudhary <142985210+Harshvardhan-To1@users.noreply.github.com>
2025-09-02 18:35:08 +0530

    Update i18n.py

--- i18n/i18n.py
+++ i18n/i18n.py
@@ -1,10 +1,12 @@
 import json
 import locale
 import os
-
+from pathlib import Path
+_CURR_DIR = Path(__file__).parent
+USED_DIR = _CURR_DIR / "locale"
 
 def load_language_list(language):
-    with open(f"./i18n/locale/{language}.json", "r", encoding="utf-8") as f:
+    with open(f"{str(USED_DIR)}/{language}.json", "r", encoding="utf-8") as f:
         language_list = json.load(f)
     return language_list
 
@@ -15,7 +17,7 @@ class I18nAuto:
             language = locale.getdefaultlocale()[
                 0
             ]  # getlocale can't identify the system's language ((None, None))
-        if not os.path.exists(f"./i18n/locale/{language}.json"):
+        if not os.path.exists(f"{str(USED_DIR)}/{language}.json"):
             language = "en_US"
         self.language = language
         self.language_map = load_language_list(language)
